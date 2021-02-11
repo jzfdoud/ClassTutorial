951347ed5c2a74133ccfadbea523f7187816448d
Jada <jada.fagan@gmail.com>
2021-02-11 09:43:31 -0500

    Add project files.

---
+++ ClassTutorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var Andrea = new Student();
+            Andrea.Name = "Andrea";
+            Andrea.Address = "4900 Parkway Dr";
+            Andrea.GPA = 4.0m;
+            Andrea.Major = "Underwater Basket Weaving";
+            Console.WriteLine($"Name: {Andrea.Name}, GPA: {Andrea.GPA}, Major: {Andrea.Major}");
+
+            var Jonathan = new Student();
+            Jonathan.Name = "Jonathan";
+            Jonathan.Address = "4900 Parkway Dr";
+            Jonathan.GPA = 4.0m;
+            Jonathan.Major = "Abovewater Basket Weaving";
+            Console.WriteLine($"Name: {Jonathan.Name}, GPA: {Jonathan.GPA}, Major: {Jonathan.Major}");
+        }
+    }
+}
---
+++ ClassTutorial/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassTutorial
+{
+    class Student
+    {
+        public string Name;
+        public string Address;
+        public decimal GPA;
+        public string Major;
+        public bool Graduated = false;
+
+    }
+}
